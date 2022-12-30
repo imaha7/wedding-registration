8d5dc1de2d99afbaf0f0ee4fa58f34382c93a8c1
ilhamogloba <ilh@ogloba.com>
2022-12-30 14:02:47 +0700

    change api url

--- utils/axios.tsx
+++ utils/axios.tsx
@@ -2,7 +2,8 @@ import axios, { AxiosRequestConfig } from "axios";
 
 // const BASE_URL = "https://pokeapi.co/api/v2";
 // const BASE_URL = "http://localhost:3010/api/v1";
-const BASE_URL = "https://wedding-registration-api.herokuapp.com/api";
+// const BASE_URL = "https://wedding-registration-api.herokuapp.com/api";
+const BASE_URL = "https://api-wedskuy.loophole.site/";
 
 const request = axios.create({
   // baseURL: process.env.REACT_APP_BACKEND_URL,
